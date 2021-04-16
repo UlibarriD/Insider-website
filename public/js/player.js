22b841c24d294617c85d5abd4514d278779728bd
Diego Ulibarri <diegoulhe@outlook.com>
2021-04-16 13:02:40 -0500

    Player Navs

--- public/js/player.js
+++ public/js/player.js
@@ -1,3 +1,18 @@
-function changeNav(){
-    var nav = document.querySelector("header");
-}
+editProfile = document.querySelectorAll(".nav")[0];
+profileStats = document.querySelectorAll(".nav")[1];
+editProfileContent = document.querySelector(".config");
+profileStatsContent = document.querySelector(".stats");
+
+editProfile.addEventListener("click", function(){
+    editProfile.classList.add("n-active");
+    profileStats.classList.remove("n-active");
+    editProfileContent.classList.add("active");
+    profileStatsContent.classList.remove("active");
+})
+
+profileStats.addEventListener("click", function(){
+    profileStats.classList.add("n-active");
+    editProfile.classList.remove("n-active");
+    profileStatsContent.classList.add("active");
+    editProfileContent.classList.remove("active");
+})
