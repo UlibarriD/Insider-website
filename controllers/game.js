eb3ceb6ba3f0f8ba7f9b97d5e473fab5ca47e5f2
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-04-28 14:11:23 -0500

    Send response when creating a game

--- controllers/game.js
+++ controllers/game.js
@@ -4,6 +4,6 @@ const Game = require("../util/database").models.game;
 // Create
 exports.postNewGame = (req, res) => {
     Game.create(req.body)
-        .then(result => console.log("Partida creada"))
-        .catch(error => console.log(error))
+        .then(result => res.send("SUCCESS"))
+        .catch(error => res.send(error))
 }
